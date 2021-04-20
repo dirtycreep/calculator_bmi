f6d7cfa7492676eb7826cb4205b823b145f99f43
Aram <Aram0070@icloud.com>
2021-04-20 16:51:51 +0400

    added functional

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 
-
+import 'package:calculator_bmi/PortraitModeMixin.dart';
+import 'package:input_calculator/input_calculator.dart';
 import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
 
 void main() => runApp(
   MaterialApp(
@@ -21,9 +23,112 @@ class HomeActivity extends StatefulWidget {
   _HomeActivityState createState() => _HomeActivityState();
 }
 
-class _HomeActivityState extends State<HomeActivity> {
+class _HomeActivityState extends State<HomeActivity> with PortraitStatefulModeMixin<HomeActivity>{
 
-  Widget CalcBtn(String Btnval){
+
+  String equation = "0";
+  String result = "0";
+  double numOne = 0;
+  String expression = "";
+  double equationFontSize = 38.0;
+  double resultFontSize = 58.0;
+
+
+  buttonPressed(String buttonText){
+    setState(() {
+
+      if (buttonText == "AC"){
+        equation = "0";
+        result = "0";
+
+      }else if(buttonText == "="){
+        expression = equation;
+        expression = expression.replaceAll("x", "*");
+        expression = expression.replaceAll("÷", "/");
+        // expression = expression.replaceAll("%", "/");
+
+        try{
+          Parser p = Parser();
+          Expression exp = p.parse(expression);
+
+          ContextModel cm = ContextModel();
+
+          result = "${exp.evaluate(EvaluationType.REAL, cm)}";
+
+        }catch(e){
+          result = "Error";
+        }
+
+
+      }  else if(buttonText == ',') {
+        if(!equation.toString().contains(',')) {
+          equation = equation.toString()+',';
+        }
+      }
+      else if(buttonText == '%') {
+          result = result + (double.parse(equation) / 100).toString();
+      }
+
+
+      else if(buttonText == '+/-') {
+        equation.toString().startsWith('-') ? equation = equation.toString().substring(1): equation = '-'+equation.toString();
+        equation = equation;
+
+      }
+      else {
+        if(equation == "0"){
+          equation = buttonText;
+        }else {
+          equation = equation + buttonText;
+        }
+      }
+
+
+    });
+
+
+
+}
+
+
+
+
+  // double _value = 0.0;
+  //
+  //
+  // set value(double value) {
+  //   setState(() {
+  //     _value = value;
+  //   });
+  // }
+
+  // Widget buildButton(String buttonText, double buttonHeight, Color buttonColor){
+  //   return Container(
+  //     height: MediaQuery.of(context).size.height * 0.1 * buttonHeight,
+  //     color: buttonColor,
+  //     child: FlatButton(
+  //         shape: RoundedRectangleBorder(
+  //             borderRadius: BorderRadius.circular(0.0),
+  //             side: BorderSide(
+  //                 color: Colors.white,
+  //                 width: 1,
+  //                 style: BorderStyle.solid
+  //             )
+  //         ),
+  //         padding: EdgeInsets.all(16.0),
+  //         onPressed: () => buttonPressed(buttonText),
+  //         child: Text(
+  //           buttonText,
+  //           style: TextStyle(
+  //               fontSize: 30.0,
+  //               fontWeight: FontWeight.normal,
+  //               color: Colors.white
+  //           ),
+  //         )
+  //     ),
+  //   );
+  // }
+  Widget CalcBtn(String buttonText){
     return Expanded(
       child: Container(
         margin: EdgeInsets.all(12),
@@ -50,27 +155,27 @@ class _HomeActivityState extends State<HomeActivity> {
         ),
         child: MaterialButton(
           padding: EdgeInsets.all(20.0),
-          child: Text(Btnval, style: TextStyle(
+          child: Text(buttonText, style: TextStyle(
             fontSize: 24.0,
             fontWeight: FontWeight.w900,
 
           ),
           ),
-          onPressed: ()=>{},
+          onPressed: ()=>buttonPressed(buttonText),
           shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(70.0)),
         ),
 
       ),
     );
   }
-  Widget CalcBtn3(String Btnval){
+  Widget CalcBtn3(String buttonText){
     return Expanded(
       child: Container(
-        margin: EdgeInsets.fromLTRB(0, 11, 0, 0),
+        margin: EdgeInsets.all(9),
         decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(60),
-            border: Border.all(color: Colors.transparent,width: 0),
-            color: Colors.grey[200],
+            borderRadius: BorderRadius.circular(30),
+            border: Border.all(color: Colors.transparent,width: 7),
+            color: Colors.orange.shade300,
             boxShadow:[
               BoxShadow(
                 color: Colors.grey[500],
@@ -91,21 +196,21 @@ class _HomeActivityState extends State<HomeActivity> {
         ),
         child: MaterialButton(
           padding: EdgeInsets.all(10.0),
-          child: Text(Btnval, style: TextStyle(
+          child: Text(buttonText, style: TextStyle(
             fontSize: 22.0,
             fontWeight: FontWeight.bold,
-            color: Colors.orange.shade300,
+            color: Colors.white,
 
           ),
           ),
-          onPressed: ()=>{},
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(70.0)),
+          onPressed: ()=>buttonPressed(buttonText),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
         ),
 
       ),
     );
   }
-  Widget CalcBtn1(String Btnval){
+  Widget CalcBtn1(String buttonText){
     return Expanded(
       child: Container(
         margin: EdgeInsets.all(8.0),
@@ -132,12 +237,12 @@ class _HomeActivityState extends State<HomeActivity> {
         ),
         child: MaterialButton(
           padding: EdgeInsets.all(30.0),
-          child: Text(Btnval, style: TextStyle(
+          child: Text(buttonText, style: TextStyle(
             fontSize: 22.0,
 
           ),
           ),
-          // onPressed: ()=>{},
+          onPressed: ()=>buttonPressed(buttonText),
           // shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(70.0)),
         ),
 
@@ -146,6 +251,13 @@ class _HomeActivityState extends State<HomeActivity> {
   }
   @override
   Widget build(BuildContext context) {
+    super.build(context);
+
+
+
+
+
+
     return SafeArea(
         child: Scaffold(
           backgroundColor: Colors.grey[200],
@@ -153,13 +265,20 @@ class _HomeActivityState extends State<HomeActivity> {
             child: Column(
               children: <Widget>[
                 Container(
-                  alignment: Alignment.bottomCenter,
-                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 41),
-                  child: Text("BMI",style: TextStyle(
-                    fontSize: 20.00,
-                    color: Colors.grey[500],
-                  ),
-                  ),
+                  child:FlatButton(onPressed: null,
+                          child: Text("",style: TextStyle(
+                            fontSize: 20.00,
+                            color: Colors.grey[500],
+                          ),
+                          )
+                  // alignment: Alignment.bottomCenter,
+                  // padding: EdgeInsets.symmetric(horizontal: 10, vertical: 41),
+                  // child: Text("",style: TextStyle(
+                  //   fontSize: 20.00,
+                  //   color: Colors.grey[500],
+                  // ),
+                  // ),
+                ),
                 ),
                 // Expanded(
                 //
@@ -173,35 +292,40 @@ class _HomeActivityState extends State<HomeActivity> {
                     child: Row(
                           crossAxisAlignment: CrossAxisAlignment.stretch,
                           children: <Widget>[
+                            // Expanded(
+                            //   child: Column(
+                            //     crossAxisAlignment: CrossAxisAlignment.stretch,
+                            //     children: <Widget>[
+                            //       Expanded(
+                            //         child: Container(
+                            //           color: Colors.red,
+                            //         ),
+                            //       ),
+                            //       Expanded(
+                            //         child: Container(
+                            //           color: Colors.yellow,
+                            //         ),
+                            //       ),
+                            //     ],
+                            //   ),
+                            // ),
                             Expanded(
                               child: Column(
                                 crossAxisAlignment: CrossAxisAlignment.stretch,
                                 children: <Widget>[
                                   Expanded(
                                     child: Container(
-                                      color: Colors.red,
-                                    ),
+                                      alignment: Alignment.centerRight,
+                                      padding: EdgeInsets.fromLTRB(10, 20, 10, 0),
+                                      child: Text(equation, style: TextStyle(fontSize: equationFontSize, color: Colors.grey[500]), ),
+                                    )
                                   ),
                                   Expanded(
                                     child: Container(
-                                      color: Colors.yellow,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                            Expanded(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.stretch,
-                                children: <Widget>[
-                                  Expanded(
-                                    child: Container(
-                                      color: Colors.purple,
-                                    ),
-                                  ),
-                                  Expanded(
-                                    child: Container(
-                                      color: Colors.black,
+                                      // color: Colors.black,
+                                      alignment: Alignment.centerRight,
+                                      padding: EdgeInsets.fromLTRB(10, 20, 10, 0),
+                                      child: Text(result, style: TextStyle(fontSize: resultFontSize, ) ),
                                     ),
                                   ),
                                 ],
@@ -277,13 +401,32 @@ class _HomeActivityState extends State<HomeActivity> {
                 ),
 
                 Expanded(
-                  flex: 4,
+                  flex: 5,
                   child: Row(
                     children: [
                       Expanded(
                         flex: 3,
                         child: Column(
                           children: <Widget>[
+                            Expanded(
+                              // flex: 1,
+
+                              child: Row(
+
+
+
+                                children: <Widget>[
+
+                                  CalcBtn3("AC"),
+                                  CalcBtn3("+/-"),
+                                  CalcBtn3("%"),
+                                  CalcBtn3("÷"),
+
+
+
+                                ],
+                              ),
+                            ),
 
 
 
@@ -299,6 +442,7 @@ class _HomeActivityState extends State<HomeActivity> {
                                         CalcBtn("7"),
                                         CalcBtn("8"),
                                         CalcBtn("9"),
+                                        CalcBtn3("x"),
 
 
 
@@ -319,6 +463,7 @@ class _HomeActivityState extends State<HomeActivity> {
                                     CalcBtn("4"),
                                     CalcBtn("5"),
                                     CalcBtn("6"),
+                                    CalcBtn3("-"),
 
 
 
@@ -340,6 +485,7 @@ class _HomeActivityState extends State<HomeActivity> {
                                     CalcBtn("1"),
                                     CalcBtn("2"),
                                     CalcBtn("3"),
+                                    CalcBtn3("+"),
 
 
 
@@ -359,9 +505,9 @@ class _HomeActivityState extends State<HomeActivity> {
 
                                   children: <Widget>[
 
-                                    CalcBtn1(""),
                                     CalcBtn("0"),
-                                    CalcBtn("."),
+                                    CalcBtn(","),
+                                    CalcBtn3("="),
 
 
 
@@ -373,74 +519,63 @@ class _HomeActivityState extends State<HomeActivity> {
 
 
 
-                            // Container(
-                            //   width: 50,
-                            //   color: Colors.blue,
-                            //
-                            // )
-
-
-
-
-
-
                           ],
                         ),
                       ),
-                      Expanded(flex: 1,
-                        child: Container(
-                          // color: Colors.grey,
-
-                          child: Column(
-                            children: <Widget>[
-
-
-                              // Row(
-
-
-                              // children: <Widget>[
-
-                              CalcBtn3("AC"),
-                              CalcBtn3("←"),
-                              CalcBtn3("GO"),
-
-
-
-                              //   ],
-                              // ),
-
-
-
-
-                              // Container(
-                              //   width: 50,
-                              //   color: Colors.blue,
-                              //
-                              // )
-
-
-
-
-
-
-                            ],
-                          ),
-                        ),
-                      ),
+                      // Expanded(flex: 1,
+                      //   child: Container(
+                      //     // color: Colors.grey,
+                      //
+                      //     child: Column(
+                      //       children: <Widget>[
+                      //
+                      //
+                      //         // Row(
+                      //
+                      //
+                      //         // children: <Widget>[
+                      //
+                      //         CalcBtn3("AC"),
+                      //         CalcBtn3("←"),
+                      //         CalcBtn3("GO"),
+                      //
+                      //
+                      //
+                      //         //   ],
+                      //         // ),
+                      //
+                      //
+                      //
+                      //
+                      //         // Container(
+                      //         //   width: 50,
+                      //         //   color: Colors.blue,
+                      //         //
+                      //         // )
+                      //
+                      //
+                      //
+                      //
+                      //
+                      //
+                      //       ],
+                      //     ),
+                      //   ),
+                      // ),
                     ],
                   ),
 
                 ),
-                Expanded(child: Row(
-                  children: <Widget>[
-                    CalcBtn1(""),
-                    CalcBtn1(""),
-                    CalcBtn1(""),
-
-                  ],
-
-                )
-                )
+                // Expanded(child: Row(
+                //   children: <Widget>[
+                //     CalcBtn1(""),
+                //     CalcBtn1(""),
+                //     CalcBtn1("h"),
+                //
+                //   ],
+                //
+                // )
+                // )
 
 
                 // Padding(padding: const EdgeInsets.only(left: 10),
@@ -471,6 +606,11 @@ class _HomeActivityState extends State<HomeActivity> {
 
         )
     );
+
+  }
+  @override
+  void dispose() {
+    super.dispose();
   }
 }
 
